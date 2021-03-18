5228d17b17c22d52cf1beca7d4f104f480da78b6
Bill <otoolew@gmail.com>
2021-03-18 12:53:22 -0400

    no message

---
+++ Assets/Code/Character/CharacterRotation.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterRotation : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed;
+    public float RotationSpeed { get => rotationSpeed; set => rotationSpeed = value; }
+
+    [SerializeField] private Vector3 offset;
+    public Vector3 Offset { get => offset; set => offset = value; }
+
+    [SerializeField] private LayerMask layerMask;
+    public LayerMask LayerMask { get => layerMask; set => layerMask = value; }
+
+    public void LookInDirection(Vector2 rotateValue)
+    {
+        if (rotateValue.sqrMagnitude < 0.01)
+            return;
+        Debug.Log(rotateValue);
+        Vector3 lookDirection = new Vector3(rotateValue.x, 0, rotateValue.y);
+        Quaternion lookRotation =  Quaternion.FromToRotation(transform.rotation.eulerAngles, lookDirection);
+        //transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, RotationSpeed * Time.deltaTime);
+        transform.eulerAngles = lookDirection;
+    }
+
+    public void LookAtPosition(Vector3 lookatpoint)
+    {
+        Vector3 playerToPoint = lookatpoint - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(playerToPoint);
+        if (lookRotation.eulerAngles != Vector3.zero)
+        {
+            lookRotation.x = 0f;
+            lookRotation.z = 0f;
+            transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, RotationSpeed * Time.deltaTime);
+        }
+    }
+
+    public void MouseLook()
+    {
+        Vector3 playerToMouse = MouseToWorldPoint(Mouse.current.position.ReadValue()) - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(playerToMouse);
+        if (lookRotation.eulerAngles != Vector3.zero)
+        {
+            lookRotation.x = 0f;
+            lookRotation.z = 0f;
+            transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, RotationSpeed * Time.deltaTime);
+        }
+    }
+
+    private Vector3 MouseToWorldPoint(Vector2 mouseScreen)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(mouseScreen);
+        ray.origin += offset;
+        if (Physics.Raycast(ray, out RaycastHit rayHit, 100.0f, layerMask))
+        {
+            return rayHit.point;
+        }
+        return transform.position;
+    }
+}
